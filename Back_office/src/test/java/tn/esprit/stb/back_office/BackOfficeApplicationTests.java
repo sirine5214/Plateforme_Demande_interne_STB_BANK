bb3658092d13c0246cc30eb20eeb21f01588b7f8
sirine5214 <sirinebenchikh206@gmail.com>
2026-08-18 17:13:02 +0200

    Back_office: abandonne Testcontainers, connexion JDBC directe au Postgres de test
    Testcontainers/docker-java (testcontainers-bom 1.20.4) ne fonctionne pas sur
    cette instance : le Docker Engine (API 1.52) rejette systematiquement l'appel
    de bootstrap en API 1.32 que docker-java envoie, sans que DOCKER_API_VERSION
    n'ait d'effet. Retour a une config JDBC simple dans
    src/test/resources/application.properties, pointant vers un vrai Postgres
    dedie et toujours actif (jenkins-backend-test-db, ajoute au
    docker-compose-jenkins.yml, reseau jenkins-network) - plus de dependance a
    Docker depuis le process de test.

--- Back_office/src/test/java/tn/esprit/stb/back_office/BackOfficeApplicationTests.java
+++ Back_office/src/test/java/tn/esprit/stb/back_office/BackOfficeApplicationTests.java
@@ -2,10 +2,8 @@ package tn.esprit.stb.back_office;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-@Import(TestcontainersConfiguration.class)
 class BackOfficeApplicationTests {
 
     @Test
